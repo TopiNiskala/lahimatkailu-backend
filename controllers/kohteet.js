6713f7aeec7fc7e126a46970d9acbdf8da6b357c
TopiNiskala <topiniskala@gmail.com>
2018-03-08 16:11:38 +0200

    Vähän nätimpi ratkaisu tai en tiedä

--- controllers/kohteet.js
+++ controllers/kohteet.js
@@ -7,11 +7,7 @@ export const index = (req, res, next) => {
 	var aLat = 60.182113;
 	var aLong = 24.913422;
 	var dist = 0.09;
-	var aLat1 = aLat + dist;
-	var aLong1 = aLong + dist;
-	var aLat2 = aLat - dist;
-	var aLong2 = aLong - dist;
-  Kohde.find({ "location.latitude": { $gt: aLat2, $lt: aLat1 }, "location.longitude": { $gt: aLong2, $lt: aLong1 }}).lean().exec((err, kohteet) => res.json(
+  Kohde.find({ "location.latitude": { $gt: (aLat - dist), $lt: (aLat + dist) }, "location.longitude": { $gt: (aLong - dist), $lt: (aLong + dist) }}).lean().exec((err, kohteet) => res.json(
     // Iterates through each 'kohde'
     { kohteet: kohteet.map(kohde => ({
       ...kohde,
